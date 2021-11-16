06ba1d9e5e1325c993cb31845b2a8d77840681e4
Nicholas Sidharta <sidharta.nic@gmail.com>
2021-11-16 21:33:59 +0700

    Dashboard update and add placeholder img

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,9 +1,134 @@
 /*Sidebar Js*/
 let btn = document.querySelector("#menu");
 let sidebar = document.querySelector(".sidebar");
-let home = document.querySelector(".home_section")
+let home = document.querySelector(".home_section");
+let recommend = document.querySelector(".recommend");
+let agenda = document.querySelector(".agenda");
+let caption = document.querySelector(".caption");
+let img = document.querySelector("img");
+let note = document.querySelector(".note_rec");
 
 btn.onclick = function(){
     sidebar.classList.toggle("active");    
     home.classList.toggle("active");
+    recommend.classList.toggle("active");
+    agenda.classList.toggle("active");
+    caption.classList.toggle("active");
+    img.classList.toggle("active");
+    note.classList.toggle("active");
 }
+
+var slidesIndex = 1;
+showSlides(slidesIndex);
+
+function slides(i){
+    showSlides(slidesIndex += i);
+}
+
+function currentSlide(i){
+    showSlides(slidesIndex = i);
+}
+
+function showSlides(i){
+    var n;
+    var slide = document.getElementsByClassName("trans");
+    var dot = document.getElementsByClassName("dot");
+    if (i > slide.length) {
+        slidesIndex = 1;
+    }
+    if (i < 1){
+        slidesIndex = slide.length;
+    }
+    for(n = 0; n < slide.length; n++){
+        slide[n].getElementsByClassName.display = "none";
+    }
+    for(n = 0; n < dot.length; n++){
+        dot[n].className = dot[n].className.replace(" active", "");
+    }
+    slide[slidesIndex-1].style.display = "block";
+    dot[slidesIndex-1].className += " dotActive";
+}
+
+//selectors
+const todoInput = document.querySelector('.todo_input');
+const todoButton = document.querySelector('.todo_button');
+const todoList = document.querySelector('.todo_list');
+const filterOption = document.querySelector('.filter_todo');
+//event listeners
+todoButton.addEventListener("click", addTodo)
+todoList.addEventListener("click", deleteCheck)
+filterOption.addEventListener("click", filterTodo)
+//functions
+
+function addTodo(event) {
+    event.preventDefault();
+    //todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //todo LI 
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo_item');
+    todoDiv.appendChild(newTodo);
+    if(todoInput.value === ""){
+        return null
+    }
+    //check mark BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete_btn')
+    todoDiv.appendChild(completedButton);
+    //delete BUTTON
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add('delete_btn')
+    todoDiv.appendChild(deleteButton);
+    //Append to Actual LIST
+    todoList.appendChild(todoDiv);
+    //Clear todo input VALUE
+    todoInput.value = ""
+}
+
+//DELETE & CHECK
+function deleteCheck(e) {
+    const item = e.target;
+    //DELETE ITEM
+    if (item.classList[0] === "delete_btn") {
+        const todo = item.parentElement;
+        //ANIMATION TRANSITION
+        todo.classList.add("fall")
+        todo.addEventListener('transitionend', function () {
+            todo.remove()
+        })
+    }
+    //COMPLETE ITEM
+    if (item.classList[0] === "complete_btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completedItem")
+    }
+}
+//FILTERING THE TASKS ACCORDING THE OPTION
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    for(let i = 0; i<todos.length; i++ ){
+        switch (e.target.value) {
+            case "all":
+                todos[i].style.display = "flex";
+                break;
+            case "completed":
+                if (todos[i].classList.contains('completedItem')) {
+                    todos[i].style.display = "flex";
+                } else {
+                    todos[i].style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todos[i].classList.contains('completedItem')) {
+                    todos[i].style.display = "flex";
+                } else {
+                    todos[i].style.display = "none";
+                }
+                break;
+        }
+    }
+} 
