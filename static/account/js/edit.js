a62cf1138ba5b7450c4fe535028925ce8d50e9de
Nicholas Sidharta <toxicancm@gmail.com>
2021-12-31 01:25:18 +0700

    edit profile page added

--- static/account/js/edit.js
+++ static/account/js/edit.js
@@ -3,9 +3,11 @@ let btn = document.querySelector("#menu");
 let sidebar = document.querySelector(".sidebar");
 let home = document.querySelector(".home_section");
 let profile = document.querySelector(".profile");
+let title = document.querySelector("h2");
 
 btn.onclick = function(){
     sidebar.classList.toggle("active");    
     home.classList.toggle("active");
     profile.classList.toggle("active");
+    title.classList.toggle("active");
 }
