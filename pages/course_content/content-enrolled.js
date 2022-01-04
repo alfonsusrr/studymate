f515196b610954fbc0d840da80e4570162477d3c
Nicholas Sidharta <toxicancm@gmail.com>
2022-01-04 16:42:13 +0700

    front-end enrolled page

--- pages/course_content/content-enrolled.js
+++ pages/course_content/content-enrolled.js
@@ -2,32 +2,20 @@
 let btn = document.querySelector("#menu");
 let sidebar = document.querySelector(".sidebar");
 let home = document.querySelector(".home_section");
-let options = document.querySelector(".options");
-let title = document.querySelector("h2");
-let display = document.querySelector(".display");
+let content = document.querySelector(".content");
 
 btn.onclick = function(){
     sidebar.classList.toggle("active");    
     home.classList.toggle("active");
-    options.classList.toggle("active");
-    title.classList.toggle("active");
-    display.classList.toggle("active");
+    content.classList.toggle("active");
 }
 
-let contact = document.querySelector(".contact");
+let enroll = document.querySelector(".enroll");
 
-contact.onclick = function(){
-    contact.classList.toggle("on");
-}
-
-let add = document.querySelector(".add");
-
-var counter = 0
-
-add.onclick = function(){
-    counter += 1
-    if(counter > 4){
-        return
+enroll.onclick = function() {
+    let ans = confirm("Are You Sure You Want to Un-Enroll?");
+    if (ans){
+        console.log("Back to unenrolled page");
+        return;
     }
-    document.getElementById("added").innerHTML += "<input class='display_text' placeholder='Enter a Text' type='text'><br>";
 }
