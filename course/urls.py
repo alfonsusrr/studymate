57768d79e7d404381c5ee97ae4e81aac26ff33c8
ArnedyNavi <alfonsus737@gmail.com>
2022-01-07 18:49:11 +0700

    Finish preview course page

--- course/urls.py
+++ course/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('preview/<id>', views.preview, name="course_preview"),
     path('search/', views.search, name="course_search"),
-    path('learn/<id>/', views.learn, name="course_learn"),
+    path('learn/<course_id>/', views.learn, name="learn_course"),
     path('mycourse/', views.my_course, name="my_course"),
     path('new/', views.make_course, name="make_course"),
-
+    path('enroll/<course_id>', views.enroll, name="enroll_course")
 ]
